71b2284d4bd2a6d21ff34b82ff593d78b29f6ab0
juanantonioqs <juanantonio19344@hotmail.com>
2021-10-20 14:56:38 +0100

    🔨Update latex template and script for report

--- process_report_by_parts.sh
+++ process_report_by_parts.sh
@@ -2,11 +2,26 @@
 
 ./process_cover.sh
 
-pandoc 8_errores.txt    \
-    --pdf-engine=xelatex                           \
-    --template=custom-report.tex                   \
-    --listings                                     \
-    -V documentclass=report                        \
-    -o ./output/book.pdf
+cp -rf ./codigo-sostenible/manuscript/* .
+
+pandoc ./codigo-sostenible/manuscript/0_pre.txt           \
+       ./codigo-sostenible/manuscript/1_intro.txt         \
+       ./codigo-sostenible/manuscript/2_flexible.txt      \
+       ./codigo-sostenible/manuscript/3_sorpresa.txt      \
+       ./codigo-sostenible/manuscript/4_cohesion.txt      \
+       ./codigo-sostenible/manuscript/5_refactoring.txt   \
+       ./codigo-sostenible/manuscript/6_principios.txt    \
+       ./codigo-sostenible/manuscript/7_srp.txt           \
+       ./codigo-sostenible/manuscript/8_errores.txt       \
+       ./codigo-sostenible/manuscript/9_tipos.txt         \
+       ./codigo-sostenible/manuscript/10_contexto.txt     \
+       ./codigo-sostenible/manuscript/11_cierre.txt       \
+       --pdf-engine=xelatex                               \
+       --template=custom-report.tex                       \
+       --listings                                         \
+       -V documentclass=report                            \
+       -o ./output/book.pdf
+
+rm -rf ./*.txt ./resources
 
 pdfunite ./output/cover.pdf ./output/book.pdf ./output/output.pdf

458b5f376382b7462b920005b94f85e6b6823d11
Maria Soria <maria.it3@gmail.com>
2021-10-21 12:41:15 +0100

    🚧 Add file to process ebooks

---
+++ process_ebook_by_parts.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#./process_cover.sh
+
+cp -rf ./codigo-sostenible/manuscript/* .
+
+pandoc ./codigo-sostenible/manuscript/0_pre.txt           \
+       ./codigo-sostenible/manuscript/1_intro.txt         \
+       ./codigo-sostenible/manuscript/2_flexible.txt      \
+       ./codigo-sostenible/manuscript/3_sorpresa.txt      \
+       ./codigo-sostenible/manuscript/4_cohesion.txt      \
+       ./codigo-sostenible/manuscript/5_refactoring.txt   \
+       ./codigo-sostenible/manuscript/6_principios.txt    \
+       ./codigo-sostenible/manuscript/7_srp.txt           \
+       ./codigo-sostenible/manuscript/8_errores.txt       \
+       ./codigo-sostenible/manuscript/9_tipos.txt         \
+       ./codigo-sostenible/manuscript/10_contexto.txt     \
+       ./codigo-sostenible/manuscript/11_cierre.txt       \
+      --metadata title="OLAKASE"                          \
+      -o ./output/ebook.epub
+
+rm -rf ./*.txt ./resources
+
+#pdfunite ./output/cover.pdf ./output/book.pdf ./output/output.pdf

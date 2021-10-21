7f33eb7d40fe35482a0304d4184f8e14dce7a26b
juanantonioqs <juanantonio19344@hotmail.com>
2021-10-21 13:41:35 +0100

    🔨💄Update script and add style for epub

--- process_ebook_by_parts.sh
+++ process_ebook_by_parts.sh
@@ -1,24 +1,23 @@
 #!/bin/sh
 
-#./process_cover.sh
-
 cp -rf ./codigo-sostenible/manuscript/* .
 
-pandoc ./codigo-sostenible/manuscript/0_pre.txt           \
-       ./codigo-sostenible/manuscript/1_intro.txt         \
-       ./codigo-sostenible/manuscript/2_flexible.txt      \
-       ./codigo-sostenible/manuscript/3_sorpresa.txt      \
-       ./codigo-sostenible/manuscript/4_cohesion.txt      \
-       ./codigo-sostenible/manuscript/5_refactoring.txt   \
-       ./codigo-sostenible/manuscript/6_principios.txt    \
-       ./codigo-sostenible/manuscript/7_srp.txt           \
-       ./codigo-sostenible/manuscript/8_errores.txt       \
-       ./codigo-sostenible/manuscript/9_tipos.txt         \
-       ./codigo-sostenible/manuscript/10_contexto.txt     \
-       ./codigo-sostenible/manuscript/11_cierre.txt       \
-      --metadata title="OLAKASE"                          \
+pandoc ./codigo-sostenible/manuscript/0_pre.txt                                           \
+       ./codigo-sostenible/manuscript/1_intro.txt                                         \
+       ./codigo-sostenible/manuscript/2_flexible.txt                                      \
+       ./codigo-sostenible/manuscript/3_sorpresa.txt                                      \
+       ./codigo-sostenible/manuscript/4_cohesion.txt                                      \
+       ./codigo-sostenible/manuscript/5_refactoring.txt                                   \
+       ./codigo-sostenible/manuscript/6_principios.txt                                    \
+       ./codigo-sostenible/manuscript/7_srp.txt                                           \
+       ./codigo-sostenible/manuscript/8_errores.txt                                       \
+       ./codigo-sostenible/manuscript/9_tipos.txt                                         \
+       ./codigo-sostenible/manuscript/10_contexto.txt                                     \
+       ./codigo-sostenible/manuscript/11_cierre.txt                                       \
+       --metadata title="Codigo Sostenible"                                               \
+       --toc                                                                              \
+       --css epub.css                                                                     \
+       --epub-cover-image ./codigo-sostenible/manuscript/resources/Codigo_Sostenible.png  \
       -o ./output/ebook.epub
 
 rm -rf ./*.txt ./resources
-
-#pdfunite ./output/cover.pdf ./output/book.pdf ./output/output.pdf

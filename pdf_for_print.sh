05b9ef5ecee7c65225cb6aac0ec1cd22744f2314
Eric Driussi <eric@leanmind.com>
2021-12-02 10:22:37 +0000

    pre-process markdowns

--- pdf_for_print.sh
+++ pdf_for_print.sh
@@ -2,7 +2,8 @@
 
 cd codigo-sostenible/manuscript
 
-find . -name "[0-9]*.txt" | sort -V | xargs    \
+find . -name "[0-9]*.txt" | sort -V | xargs  cat |\
+sed -Ee 's/(^#)/\n\1/' -Ee 's/(```)(.+)$/\1{title="\2"}/' -Ee 's/(\[.+\]) (\(.+\))/\1\2/' |\
   pandoc                                       \
     --pdf-engine=xelatex                       \
     --template=../../custom-book.tex           \

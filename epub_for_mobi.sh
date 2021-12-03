ba60b84396aebd40864edb3bd3f6031d79cbc60e
Eric Driussi <eric@leanmind.com>
2021-12-03 19:22:08 +0000

    fixed sed regex

--- epub_for_mobi.sh
+++ epub_for_mobi.sh
@@ -2,7 +2,18 @@
 
 cd codigo-sostenible/manuscript
 
-pandoc .tempBook\
+# Prepare markdown for processing
+
+# Sort all chapters and cat them to stdout
+find . -name "[0-9]*.txt" | sort -V | xargs  cat |\
+
+# Ensure: h1 headers work, links respect md format, code block languages are passed as capitalized titles
+sed -Ee 's:(^#):\n\1:' \
+    -Ee 's:] \(:](:g' \
+    -Ee 's:(```)(.+)$:\1{title=\u\2}:' |\
+
+# Run Pandoc on stdin
+pandoc \
     --toc                                                 \
     --css ../../epub.css                                  \
     --highlight-style ../../monochrome.theme              \

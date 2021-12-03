d130b0c0ab57126729611c0837e3695e686f2840
Eric Driussi <eric@leanmind.com>
2021-12-03 19:49:27 +0000

    make pandoc recognize yml for epub

--- epub.sh
+++ epub.sh
@@ -4,8 +4,8 @@ cd codigo-sostenible/manuscript
 
 # Prepare markdown for processing
 
-# Sort all chapters and cat them to stdout
-find . -name "[0-9]*.txt" | sort -V | xargs  cat |\
+# Sort all chapters and cat them to stdout (Ignore "xargs: 'cat' terminated by signal 13" known bug)
+find . -name "[0-9]*.txt" | sort -V | xargs  cat 2>/dev/null |\
 
 # Ensure: h1 headers work, links respect md format, code block languages are passed as capitalized titles
 sed -Ee 's:(^#):\n\1:' \
@@ -18,5 +18,5 @@ pandoc \
     --css ../../epub.css                                  \
     --epub-cover-image ./resources/Codigo_Sostenible.png  \
     -o ./../../output/ebook.epub                          \
-    ../../metadata.yml                                    \
+    --metadata-file ../../metadata.yml                    \
 && echo "EPUB successfully generated"
--- epub_for_mobi.sh
+++ epub_for_mobi.sh
@@ -4,8 +4,8 @@ cd codigo-sostenible/manuscript
 
 # Prepare markdown for processing
 
-# Sort all chapters and cat them to stdout
-find . -name "[0-9]*.txt" | sort -V | xargs  cat |\
+# Sort all chapters and cat them to stdout (Ignore "xargs: 'cat' terminated by signal 13" known bug)
+find . -name "[0-9]*.txt" | sort -V | xargs  cat 2>/dev/null |\
 
 # Ensure: h1 headers work, links respect md format, code block languages are passed as capitalized titles
 sed -Ee 's:(^#):\n\1:' \
@@ -19,5 +19,5 @@ pandoc \
     --highlight-style ../../monochrome.theme              \
     --epub-cover-image ./resources/Codigo_Sostenible.png  \
     -o ./../../output/ebook_for_mobi.epub                 \
-    ../../metadata.yml                                    \
+    --metadata-file ../../metadata.yml                    \
 && echo "EPUB for MOBI successfully generated"

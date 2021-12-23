f7c8977602a0651c533d73ce47cd462b804a410a
Manuel Perez Regalado <manuel@leanmind.es>
2021-12-23 11:14:35 +0000

    update screen version

--- pdf_for_screen.sh
+++ pdf_for_screen.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+mkdir output/tmp
 cd codigo-sostenible/manuscript
 
 # Prepare markdown for processing
+xelatex -output-directory ./../../output/tmp ../../screen/starting.tex
+xelatex -output-directory ./../../output/tmp ../../screen/copyright.tex
 
 # Sort all chapters and cat them to stdout
 find . -name "[0-9]*.txt" | sort -V | xargs  cat |\
@@ -15,9 +18,15 @@ sed -Ee 's:(^#):\n\1:' \
 # Run Pandoc on stdin
 pandoc \
     --pdf-engine=xelatex                       \
-    --template=../../custom-report.tex         \
+    --template=../../screen/custom-report.tex  \
     --listings                                 \
     -V documentclass=report                    \
     -f markdown-implicit_figures               \
-    -o ./../../output/book_for_screen.pdf      \
+    -o ./../../output/tmp/tmp_book_for_screen.pdf      \
 && echo "PDF for screen successfully generated"
+
+pandoc --pdf-engine=xelatex --template=../../screen/ending.tex --listings -V documentclass=report -f markdown-implicit_figures -o ./../../output/tmp/ending.pdf agradecimientos.txt autor.txt bibliografia.txt savvily.txt
+
+pdfunite ./../../output/tmp/starting.pdf ./../../output/tmp/tmp_book_for_screen.pdf ./../../output/tmp/copyright.pdf ./../../output/tmp/ending.pdf ./../../output/book_for_screen.pdf && echo "PDF for screen successfully generated"
+
+rm -rf ../../output/tmp

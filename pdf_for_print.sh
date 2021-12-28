a801433b6955db428ff0a23513527131f74e353b
Manuel Perez Regalado <manuel@leanmind.es>
2021-12-28 15:18:34 +0000

    lint code

--- pdf_for_print.sh
+++ pdf_for_print.sh
@@ -2,31 +2,39 @@
 
 mkdir output/tmp
 cd codigo-sostenible/manuscript
-
 # Prepare book beginning and ending pages separately
 xelatex -output-directory ./../../output/tmp ../../print/starting.tex
 
-# Prepare markdown for processing
-
+# Prepare MD
 # Sort all chapters and cat them to stdout
-find . -name "[0-9]*.txt" | sort -V | xargs  cat |\
-
+find . -name "[0-9]*.txt" | sort -V | xargs  cat | \
 # Ensure: h1 headers work, links respect md format, code block languages are passed as capitalized titles
-sed -Ee 's:(^#):\n\1:' \
-    -Ee 's:] \(:](:g' \
-    -Ee 's:(```)(.+)$:\1{title=\u\2}:' |\
+sed -Ee 's:(^#):\n\1:' -Ee 's:] \(:](:g' -Ee 's:(```)(.+)$:\1{title=\u\2}:' | \
 
 # Run Pandoc on stdin
 pandoc \
-    --pdf-engine=xelatex                       \
-    --template=../../print/custom-book.tex           \
-    --listings                                 \
-    -V documentclass=book                      \
-    -f markdown-implicit_figures               \
+    --pdf-engine=xelatex \
+    --template=../../print/custom-book.tex \
+    --listings \
+    -V documentclass=book \
+    -f markdown-implicit_figures \
     -o ./../../output/tmp/tmp_book_for_print.pdf
 
-pandoc --pdf-engine=xelatex --template=../../print/ending.tex --listings -V documentclass=book -f markdown-implicit_figures -o ./../../output/tmp/ending.pdf agradecimientos.txt autor.txt bibliografia.txt savvily.txt
+pandoc \
+    --pdf-engine=xelatex \
+    --template=../../print/ending.tex \
+    --listings -V documentclass=book \
+    -f markdown-implicit_figures \
+    -o ./../../output/tmp/ending.pdf agradecimientos.txt autor.txt bibliografia.txt savvily.txt
 
-gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=./../../output/book_for_print.pdf ./../../output/tmp/starting.pdf ./../../output/tmp/tmp_book_for_print.pdf ./../../output/tmp/ending.pdf && echo "PDF for print successfully generated"
+gs \
+    -q \
+    -dNOPAUSE \
+    -dBATCH \
+    -sDEVICE=pdfwrite \
+    -sOutputFile=./../../output/book_for_print.pdf \
+    ./../../output/tmp/starting.pdf ./../../output/tmp/tmp_book_for_print.pdf ./../../output/tmp/ending.pdf
+
+echo "PDF for print successfully generated"
 
 rm -rf ../../output/tmp
--- pdf_for_screen.sh
+++ pdf_for_screen.sh
@@ -5,26 +5,35 @@ cd codigo-sostenible/manuscript
 
 # Prepare markdown for processing
 xelatex -output-directory ./../../output/tmp ../../screen/starting.tex
-
 # Sort all chapters and cat them to stdout
-find . -name "[0-9]*.txt" | sort -V | xargs  cat |\
+find . -name "[0-9]*.txt" | sort -V | xargs  cat | \
 
 # Ensure: h1 headers work, links respect md format, code block languages are passed as capitalized titles
-sed -Ee 's:(^#):\n\1:' \
-    -Ee 's:] \(:](:g' \
-    -Ee 's:(```)(.+)$:\1{title=\u\2}:' |\
+sed -Ee 's:(^#):\n\1:' -Ee 's:] \(:](:g' -Ee 's:(```)(.+)$:\1{title=\u\2}:' | \
 
 # Run Pandoc on stdin
 pandoc \
-    --pdf-engine=xelatex                       \
-    --template=../../screen/custom-report.tex  \
-    --listings                                 \
-    -V documentclass=report                    \
-    -f markdown-implicit_figures               \
-    -o ./../../output/tmp/tmp_book_for_screen.pdf      \
+    --pdf-engine=xelatex \
+    --template=../../screen/custom-report.tex \
+    --listings \
+    -V documentclass=report \
+    -f markdown-implicit_figures \
+    -o ./../../output/tmp/tmp_book_for_screen.pdf \
 
-pandoc --pdf-engine=xelatex --template=../../screen/ending.tex --listings -V documentclass=report -f markdown-implicit_figures -o ./../../output/tmp/ending.pdf agradecimientos.txt autor.txt bibliografia.txt savvily.txt
+pandoc \
+    --pdf-engine=xelatex\
+    --template=../../screen/ending.tex\
+    --listings -V documentclass=report\
+    -f markdown-implicit_figures\
+    -o ./../../output/tmp/ending.pdf agradecimientos.txt autor.txt bibliografia.txt savvily.txt
 
-gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=./../../output/book_for_screen.pdf ./../../output/tmp/starting.pdf ./../../output/tmp/tmp_book_for_screen.pdf ./../../output/tmp/ending.pdf && echo "PDF for screen successfully generated"
+gs \
+  -q \
+  -dNOPAUSE \
+  -dBATCH \
+  -sDEVICE=pdfwrite \
+  -sOutputFile=./../../output/book_for_screen.pdf \
+  ./../../output/tmp/starting.pdf ./../../output/tmp/tmp_book_for_screen.pdf ./../../output/tmp/ending.pdf
+echo "PDF for screen successfully generated"
 
 rm -rf ../../output/tmp

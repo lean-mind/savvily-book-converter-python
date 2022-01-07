c034cb36846c60cf884ab5a5fc604e7836cacba5
Eric Driussi <eric@leanmind.com>
2022-01-07 19:08:21 +0000

    make script more resilient

--- convert.sh
+++ convert.sh
@@ -5,47 +5,42 @@ if [ $# -lt 2 ]; then
     exit 1
 fi
 
-ARGS=$(getopt --options psea --long "print,screen,epub,all" -- "$@")
-
 case "$1" in
   -p|--print)
-    mkdir -p .manuscript && cp -r $2/* ./.manuscript
+    mkdir -p .manuscript && cp -r "$2"/* ./.manuscript
     docker run -it --rm \
-      --volume $PWD:/data \
-      -u $(id -u ${USER}):$(id -g ${USER}) \
+      --volume "$PWD":/data \
+      -u "$(id -u "$USER"):$(id -g "$USER")" \
       --entrypoint ./src/scripts/pdf_for_print.sh \
       savvily-book-generator
     shift;;
 
   -s|--screen)
-    mkdir -p .manuscript && cp -r $2/* ./.manuscript
+    mkdir -p .manuscript && cp -r "$2"/* ./.manuscript
     docker run -it --rm \
-      --volume $PWD:/data \
-      -u $(id -u ${USER}):$(id -g ${USER}) \
+      --volume "$PWD":/data \
+      -u "$(id -u "$USER"):$(id -g "$USER")" \
       --entrypoint ./src/scripts/pdf_for_screen.sh \
       savvily-book-generator
     shift;;
 
   -e|--epub)
-    mkdir -p .manuscript && cp -r $2/* ./.manuscript
+    mkdir -p .manuscript && cp -r "$2"/* ./.manuscript
     docker run -it --rm \
-      --volume $PWD:/data \
-      -u $(id -u ${USER}):$(id -g ${USER}) \
+      --volume "$PWD":/data \
+      -u "$(id -u "$USER"):$(id -g "$USER")" \
       --entrypoint ./src/scripts/epub.sh \
       savvily-book-generator
     docker run -it --rm \
-      --volume $PWD:/data \
-      -u $(id -u ${USER}):$(id -g ${USER}) \
+      --volume "$PWD":/data \
+      -u "$(id -u "$USER"):$(id -g "$USER")" \
       --entrypoint ./src/scripts/epub_for_mobi.sh \
       savvily-book-generator
     shift;;
 
   -a|--all)
-    ./convert.sh -p $2; ./convert.sh -s $2; ./convert.sh -e $2
+    ./convert.sh -p "$2"; ./convert.sh -s "$2"; ./convert.sh -e "$2"
     shift;;
-
-  --)
-    break;;
 
    *)
     printf "Unknown option %s\n" "$1"

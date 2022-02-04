0b005517f79701ab3e6f9c4ff76e49e4da02f367
Eric Driussi <eric@leanmind.com>
2022-02-04 12:42:08 +0000

    hopefully clearer now

--- src/scripts/pdf.sh
+++ src/scripts/pdf.sh
@@ -12,8 +12,11 @@ xelatex -output-directory ./../output/.tmp ../src/templates/"$outputType"/openin
 # Sort all chapters and cat them to stdout
 find . -maxdepth 1 -name "[0-9]*.txt" -o -name '[0-9]*.md' | sort -V | xargs  cat | \
 
-# Ensure: h1 headers work, links respect md format, code block languages are passed as capitalized titles
-sed -Ee 's:(^#):\n\1:' -Ee 's:] \(:](:g' -Ee 's:(```)(.+)$:\1{title=\u\2}:' -Ee '/!.*/! s:(.+?)\[(.+?)\]\(([^)]+)\)(.+?):\1\2[^\3]\4\n\n\n[^\3]\: \3\n:g' -Ee 's:\s\[\^:\[\^:g' | \
+# Ensure: correct h1 headers, links respect md format, code block languages are passed as capitalized titles, refs have
+# no separation with previous word
+sed -Ee 's:(^#):\n\1:' -Ee 's:] \(:](:g' -Ee 's:(```)(.+)$:\1{title=\u\2}:' -Ee 's:\s\[\^:\[\^:g' | \
+# All non-images links ([some](text)) are converted to footnotes, takes the links as refs to avoid spaces and duplicates
+sed -E '/!.*/! s:(.+?)\[(.+?)\]\(([^)]+)\)(.+?):\1\2[^\3]\4\n\n\n[^\3]\: \3\n:g' | \
 
 pandoc \
     --pdf-engine=xelatex \

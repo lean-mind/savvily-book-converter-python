3cf99c89140c4d3cfe04370d4e6cbd0041eeb099
Manuel Pérez Regalado <perezregaladomanuel@gmail.com>
2022-02-11 11:25:05 +0000

    Remove toc from epub

--- src/scripts/epub.sh
+++ src/scripts/epub.sh
@@ -7,7 +7,6 @@ formattedManuscript=$(manuscriptFormatter "ebook")
 
 # Run Pandoc on formatted input
 echo "$formattedManuscript" | pandoc \
-    --toc \
     --epub-embed-font=/usr/share/fonts/Roboto-Bold.ttf \
     --css ../src/templates/epub/epub.css \
     --epub-cover-image ./resources/book-cover-print.png \

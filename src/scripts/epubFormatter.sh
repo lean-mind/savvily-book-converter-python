7301e1afa462e2db0aa4c53ff13df623dcc05e72
juanantonioqs <juanantonio19344@hotmail.com>
2022-04-08 09:46:55 +0100

    add watermark for epub

--- src/scripts/epubFormatter.sh
+++ src/scripts/epubFormatter.sh
@@ -15,6 +15,18 @@ cd ./EPUB/text/
 
 find . -maxdepth 1 -name "ch*.xhtml" -exec sed -i '' -E 's/<pre title="([A-z]*#?)".*/<code class="language">\1<\/code>\n&/' {} \;
 
+cp ../../../../src/user-details-for-watermark.txt .
+
+add_watermark_for_epub() {
+  pathUserDetails=./user-details.txt
+  name=$(cut -d ';' -f1 $pathUserDetails)
+  email=$(cut -d ';' -f2 $pathUserDetails)
+  dni=$(cut -d ';' -f3 $pathUserDetails)
+  find . -maxdepth 1 -name "ch*.xhtml" -exec sed -i '' -E 's/<\/body>/<br\/><br\/><section class='\"section-user-data\"'><p>'"$name"'<\/p><p>'"$email"'<\/p><p>'"$dni"'<\/p><\/section>\n&/' {} \;
+}
+
+add_watermark_for_epub
+
 cd ../..
 
 zip ebook.zip -r EPUB META-INF mimetype

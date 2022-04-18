0e6703077c817854091479687b7594dfb1578fd7
juanantonioqs <juanantonio19344@hotmail.com>
2022-04-18 11:05:52 +0100

    Modify epubFormatter.sh to generate epub with watermark for each user

--- src/scripts/epubFormatter.sh
+++ src/scripts/epubFormatter.sh
@@ -3,38 +3,50 @@ set -e
 
 cd ./output
 
-mv ebook.epub ebook.zip
-
-mkdir epub-version
-
-mv ebook.zip ./epub-version && cd ./epub-version
-
-unzip ebook.zip && rm ebook.zip
-
-cd ./EPUB/text/
-
-find . -maxdepth 1 -name "ch*.xhtml" -exec sed -i '' -E 's/<pre title="([A-z]*#?)".*/<code class="language">\1<\/code>\n&/' {} \;
-
-cp ../../../../src/user-details-for-watermark.txt .
+pathUserDetails="../src/user-details-for-watermark.txt"
 
 add_watermark_for_epub() {
-  pathUserDetails=./user-details-for-watermark.txt
-  name=$(cut -d ';' -f1 $pathUserDetails)
-  email=$(cut -d ';' -f2 $pathUserDetails)
-  dni=$(cut -d ';' -f3 $pathUserDetails)
+  userInformation=$1
+  name=$(echo $userInformation | cut -d ';' -f1)
+  email=$(echo $userInformation | cut -d ';' -f2)
+  dni=$(echo $userInformation | cut -d ';' -f3)
   find . -maxdepth 1 -name "ch*.xhtml" -exec sed -i '' -E 's/<\/body>/<br\/><br\/><section class='\"section-user-data\"'><p>'"$name"'<\/p><p>'"$email"'<\/p><p>'"$dni"'<\/p><\/section>\n&/' {} \;
 }
 
-add_watermark_for_epub
+while read -r line; do
+  name=$(echo "$line" | cut -d ';' -f1)
+  username="$(echo $name | sed 's/ /_/g')"
 
-cd ../..
+  if [ -d ./epub-version-"$username"/ ]; then
+    rm -rf ./epub-version-"$username"/
+  fi
 
-zip ebook.zip -r EPUB META-INF mimetype
+  cp ebook.epub ebook_"$username".epub
 
-mv ebook.zip ebook.epub
+  mv ebook_"$username".epub ebook_"$username".zip
 
-mv ebook.epub ../
+  mkdir epub-version-"$username"
 
-cd ..
+  mv ebook_"$username".zip ./epub-version-"$username" && cd ./epub-version-"$username"
 
-rm -rf ./epub-version
+  unzip ebook_"$username".zip && rm ebook_"$username".zip
+
+  cd ./EPUB/text/
+
+  find . -maxdepth 1 -name "ch*.xhtml" -exec sed -i '' -E 's/<pre title="([A-z]*#?)".*/<code class="language">\1<\/code>\n&/' {} \;
+
+  add_watermark_for_epub "$line"
+
+  cd ../..
+
+  zip ebook_"$username".zip -r EPUB META-INF mimetype
+
+  mv ebook_"$username".zip ebook_"$username".epub
+
+  mv ebook_"$username".epub ../
+
+  cd ..
+
+  rm -rf ./epub-version-"$username"
+
+done < "$pathUserDetails"

ce22efb6c261b19f4628b9dfe8dfce820c50729d
Eric Driussi <eric@leanmind.com>
2022-05-12 16:58:13 +0100

    handle errors

--- src/scripts/epub.py
+++ src/scripts/epub.py
@@ -9,6 +9,9 @@ os.chdir('.tmp-manuscript')
 formattingCommand = manuscriptFormatter.getFormattedManuscriptStreamFor("ebook")
 
 pandocEpubCommand = 'timeout 600 pandoc --epub-embed-font=/usr/share/fonts/Roboto-Bold.ttf --css ../src/templates/epub/epub.css --epub-cover-image ./resources/book-cover-print.png -o ./../output/ebook.epub --metadata-file ../src/templates/epub/metadata.yml'
-subprocess.run(pandocEpubCommand, stdin=formattingCommand, shell=True)
-
-sys.exit(0)
+try:
+    subprocess.run(pandocEpubCommand, stdin=formattingCommand, check=True, shell=True)
+    sys.exit(0)
+except subprocess.CalledProcessError as e:
+    print('[ERROR]:', e)
+    sys.exit(1)

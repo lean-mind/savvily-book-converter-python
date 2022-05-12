c758270caf653dd0a80715ff43fa47b3d06dd128
Eric Driussi <eric@leanmind.com>
2022-05-12 18:47:06 +0100

    refactor epub script

--- src/scripts/epub.py
+++ src/scripts/epub.py
@@ -3,15 +3,29 @@ import sys
 import os
 import manuscriptFormatter
 
-subprocess.run(['mkdir', '-p', 'output'])
-os.chdir('.tmp-manuscript')
 
-formattingCommand = manuscriptFormatter.getFormattedManuscriptStreamFor("ebook")
+def buildPandocCommand():
+    metadata = '--metadata-file=../src/templates/epub/metadata.yml'
+    output = '--output=./../output/ebook.epub'
+    css = '--css=../src/templates/epub/epub.css'
+    coverImage = '--epub-cover-image=./resources/book-cover-print.png'
+    font = '--epub-embed-font=/usr/share/fonts/Roboto-Bold.ttf'
+    return ['timeout', '600', 'pandoc', font, css, coverImage, output, metadata]
 
-pandocEpubCommand = 'timeout 600 pandoc --epub-embed-font=/usr/share/fonts/Roboto-Bold.ttf --css ../src/templates/epub/epub.css --epub-cover-image ./resources/book-cover-print.png -o ./../output/ebook.epub --metadata-file ../src/templates/epub/metadata.yml'
-try:
-    subprocess.run(pandocEpubCommand, stdin=formattingCommand, check=True, shell=True)
-    sys.exit(0)
-except subprocess.CalledProcessError as e:
-    print('[ERROR]:', e)
-    sys.exit(1)
+
+def createEpubFromStream(formattedStream):
+    subprocess.run(buildPandocCommand(), stdin=formattedStream, check=True)
+
+
+if __name__ == "__main__":
+    try:
+        os.makedirs('output', exist_ok=True)
+        os.chdir('.tmp-manuscript')
+        createEpubFromStream(manuscriptFormatter.getFormattedManuscriptStreamFor("ebook"))
+        sys.exit(0)
+    except subprocess.CalledProcessError as e:
+        print('[ERROR]: Pandoc command failed!\n', e)
+        sys.exit(1)
+    except Exception as e:
+        print('[ERROR]: Something went wrong!\n', e)
+        sys.exit(1)

88d33a65bbab681ff6bdfdc085eb6aa556e4e2ad
Eric Driussi <eric@leanmind.com>
2022-05-12 08:45:16 +0100

    manage streams instead of commands

--- src/scripts/epub.py
+++ src/scripts/epub.py
@@ -9,6 +9,6 @@ os.chdir('.tmp-manuscript')
 formattingCommand = manuscriptFormatter.getCommandFor("ebook")
 
 pandocEpubCommand = 'timeout 600 pandoc --epub-embed-font=/usr/share/fonts/Roboto-Bold.ttf --css ../src/templates/epub/epub.css --epub-cover-image ./resources/book-cover-print.png -o ./../output/ebook.epub --metadata-file ../src/templates/epub/metadata.yml'
-subprocess.run(pandocEpubCommand, stdin=formattingCommand.stdout, shell=True)
+subprocess.run(pandocEpubCommand, stdin=formattingCommand, shell=True)
 
 sys.exit(0)
--- src/scripts/manuscriptFormatter.py
+++ src/scripts/manuscriptFormatter.py
@@ -3,11 +3,13 @@ import subprocess
 
 def getCommandFor(type: str):
   sortCommand = "find . -maxdepth 1 -name '[0-9]*.txt' -o -name '[0-9]*.md' | sort -V | xargs cat"
+  manuscriptStream = subprocess.Popen(sortCommand, stdout=subprocess.PIPE, shell=True).stdout
+
   formatCommand = r"sed -Ee 's:(^#):\n\1:' -Ee 's:] \(:](:g' -Ee 's:(```)(.+)$:\1{title=\u\2}:' -Ee 's:\s\[\^:\[\^:g'"
+  formattedManuscriptStream = subprocess.Popen(formatCommand, stdin=manuscriptStream, stdout=subprocess.PIPE, shell=True).stdout
 
   if type == "print":
     linksAsFootnotes = r'sed -E "/!.*/! s:(.+?)\[(.....+?)\]\(([^)]+)\)(.+?):\1\2[^\3]\4\n\n[^\3]\: \3\n:g'
-    formatCommand = r'%s | %s' % (formatCommand, linksAsFootnotes)
+    return subprocess.Popen(linksAsFootnotes, stdin=formattedManuscriptStream, stdout=subprocess.PIPE, shell=True).stdout
 
-  command = '%s | %s' % (sortCommand, formatCommand)
-  return subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+  return formattedManuscriptStream

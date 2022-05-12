20ef86462f9d84c1dc1b5d7e0c62be27060ea0e2
Eric Driussi <eric@leanmind.com>
2022-05-12 08:37:17 +0100

    make formatter return a stream

--- src/scripts/epub.py
+++ src/scripts/epub.py
@@ -7,9 +7,8 @@ subprocess.run(['mkdir', '-p', 'output'])
 os.chdir('.tmp-manuscript')
 
 formattingCommand = manuscriptFormatter.getCommandFor("ebook")
-formattedPandocInput = subprocess.Popen(formattingCommand, stdout=subprocess.PIPE, shell=True)
 
 pandocEpubCommand = 'timeout 600 pandoc --epub-embed-font=/usr/share/fonts/Roboto-Bold.ttf --css ../src/templates/epub/epub.css --epub-cover-image ./resources/book-cover-print.png -o ./../output/ebook.epub --metadata-file ../src/templates/epub/metadata.yml'
-subprocess.run(pandocEpubCommand, stdin=formattedPandocInput.stdout, shell=True)
+subprocess.run(pandocEpubCommand, stdin=formattingCommand.stdout, shell=True)
 
 sys.exit(0)
--- src/scripts/manuscriptFormatter.py
+++ src/scripts/manuscriptFormatter.py
@@ -1,8 +1,13 @@
+import subprocess
+
+
 def getCommandFor(type: str):
-  sortedManuscript = "find . -maxdepth 1 -name '[0-9]*.txt' -o -name '[0-9]*.md' | sort -V | xargs cat"
-  formattedManuscript = r"sed -Ee 's:(^#):\n\1:' -Ee 's:] \(:](:g' -Ee 's:(```)(.+)$:\1{title=\u\2}:' -Ee 's:\s\[\^:\[\^:g'"
+  sortCommand = "find . -maxdepth 1 -name '[0-9]*.txt' -o -name '[0-9]*.md' | sort -V | xargs cat"
+  formatCommand = r"sed -Ee 's:(^#):\n\1:' -Ee 's:] \(:](:g' -Ee 's:(```)(.+)$:\1{title=\u\2}:' -Ee 's:\s\[\^:\[\^:g'"
 
   if type == "print":
-      formattedManuscript = r'%s | sed -E "/!.*/! s:(.+?)\[(.....+?)\]\(([^)]+)\)(.+?):\1\2[^\3]\4\n\n[^\3]\: \3\n:g' % (formattedManuscript)
+    linksAsFootnotes = r'sed -E "/!.*/! s:(.+?)\[(.....+?)\]\(([^)]+)\)(.+?):\1\2[^\3]\4\n\n[^\3]\: \3\n:g'
+    formatCommand = r'%s | %s' % (formatCommand, linksAsFootnotes)
 
-  return '%s | %s' % (sortedManuscript, formattedManuscript)
+  command = '%s | %s' % (sortCommand, formatCommand)
+  return subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)

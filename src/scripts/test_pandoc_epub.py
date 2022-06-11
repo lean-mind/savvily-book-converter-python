84aca1e1d9ac91d1f3c5a1f393bf28cc3d59e6a5
Eric Driussi <eric@leanmind.com>
2022-06-11 19:17:19 +0100

    basic structure for testing epub format

---
+++ src/scripts/test_pandoc_epub.py
@@ -0,0 +1,31 @@
+from ebooklib import epub
+import zipfile as epub_explorer
+
+epub_path = '../output/ebook.epub'
+book = epub.read_epub(epub_path)
+
+
+def chapters_href() -> list:
+    full_content = epub_explorer.ZipFile(epub_path).namelist()
+    chapters = []
+    for ebook_item in full_content:
+        if (ebook_item.startswith('EPUB/text/ch')):
+            chapters.append(ebook_item.replace('EPUB/', ''))
+    return chapters
+
+
+class Test_pandoc_epub_output:
+
+    class Test_headings:
+        first_chapter_href: str = chapters_href()[0]
+        chapter_one = book.get_item_with_href(first_chapter_href)
+        chapter_one_text: str = chapter_one.get_content().decode()
+        h1_headings: str = '\n'.join(line for line in chapter_one_text.splitlines() if (line.startswith('<h1')))
+
+        def test_only_one_h1_header_in_first_chapter(self):
+            number_of_h1_headings = self.h1_headings.split('\n')
+            assert len(number_of_h1_headings) == 1
+
+        def test_h1_header_works(self):
+            expected_h1 = '>Est versat sed remorum ordine murice<'
+            assert expected_h1 in self.h1_headings

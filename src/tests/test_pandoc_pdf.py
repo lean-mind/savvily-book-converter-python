3b16619933f67d9fae227f9c47b4053db29572b9
Eric Driussi <eric@leanmind.com>
2022-07-07 16:49:30 +0100

    restructure pdf render test

--- src/tests/test_pandoc_pdf.py
+++ src/tests/test_pandoc_pdf.py
@@ -1,14 +1,16 @@
+import subprocess
 from pdfminer.high_level import extract_text
+from pdfminer.utils import AnyIO
 
 
 class TestPandocPdfOutput:
-    raw_text = extract_text(
-        "output/codigo_sostenible_savvily/codigo_sostenible_savvily.pdf"
-    )
-    text_without_empty_lines = [line for line in raw_text.splitlines() if line]
+    raw_pdf: AnyIO
+    subprocess.run(["./convert.sh", "-p"], check=True)
+    full_text = extract_text("output/python_print.pdf")
+    raw_pdf = [line for line in full_text.splitlines() if line]
 
     def test_total_page_number(self):
-        last_line = self.text_without_empty_lines[-1]
+        last_line = self.raw_pdf[-1]
         expected_number_of_pages = 19
         number_of_closing_blank_pages = 3
         assert (
@@ -16,9 +18,7 @@ class TestPandocPdfOutput:
         )
 
     def test_code_block_lang_capitalized(self):
-        code_block_tags = [
-            tag for tag in self.text_without_empty_lines if tag.lower() == "java"
-        ]
+        code_block_tags = [tag for tag in self.raw_pdf if tag.lower() == "java"]
         lower_cased_tags = [
             lower for lower in code_block_tags if not lower[0].isupper()
         ]

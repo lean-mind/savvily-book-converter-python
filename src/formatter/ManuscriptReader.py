538eccc848d2273c6c38cbafb8defd42fbc0acaf
Eric Driussi <eric@leanmind.com>
2022-07-12 13:24:19 +0100

    tests and implemetation for basic reader

--- src/formatter/ManuscriptReader.py
+++ src/formatter/ManuscriptReader.py
@@ -3,12 +3,31 @@ import re
 
 
 class ManuscriptReader:
-    # def __init__(self):
+    def __init__(self, path: str):
+        self.path = path
 
-    # def read(self):
+    def read(self) -> str:
+        manuscript_files = self.get_sorted_chapters()
+        chapters = []
+        for file in manuscript_files:
+            with open(f"{self.path}/{file}", "r") as f:
+                chapters.append(self._list_to_string(f.readlines()))
+        book_as_string = self._list_to_string(chapters)
+        return book_as_string
 
-    def get_sorted_chapters(self, path: str) -> list:
-        dir_list = os.listdir(path)
+    def read_chapters(self, chapters: list) -> str:
+        read_chapters = []
+        for file in chapters:
+            with open(f"{self.path}/{file}", "r") as f:
+                read_chapters.append(self._list_to_string(f.readlines()))
+        chapters_as_string = self._list_to_string(read_chapters)
+        return chapters_as_string
+
+    def get_sorted_chapters(self) -> list:
+        dir_list = os.listdir(self.path)
         return sorted(
             [file for file in dir_list if re.match(r"\d\d_.*\.md|\d\d_.*\.txt", file)]
         )
+
+    def _list_to_string(self, string_list: list) -> str:
+        return "".join(string_list)

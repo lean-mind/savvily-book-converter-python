c4b82bc76cbbadc3cda6c6bc1716b6be05b2cd2a
Eric Driussi <eric@leanmind.com>
2022-07-12 17:01:41 +0100

    naming

--- src/formatter/ManuscriptReader.py
+++ src/formatter/ManuscriptReader.py
@@ -7,27 +7,25 @@ class ManuscriptReader:
         self.path = path
 
     def read(self) -> str:
-        manuscript_files = self.get_sorted_chapters()
-        chapters = []
-        for file in manuscript_files:
-            with open(f"{self.path}/{file}", "r") as f:
-                chapters.append(self._list_to_string(f.readlines()))
-        book_as_string = self._list_to_string(chapters)
-        return book_as_string
-
-    def read_chapters(self, chapters: list) -> str:
+        chapters_filenames = self.get_sorted_chapters()
         read_chapters = []
-        for file in chapters:
+        for file in chapters_filenames:
             with open(f"{self.path}/{file}", "r") as f:
                 read_chapters.append(self._list_to_string(f.readlines()))
-        chapters_as_string = self._list_to_string(read_chapters)
-        return chapters_as_string
+        return self._list_to_string(read_chapters)
+
+    def read_chapters(self, chapters_filenames: list) -> str:
+        read_chapters = []
+        for file in chapters_filenames:
+            with open(f"{self.path}/{file}", "r") as f:
+                read_chapters.append(self._list_to_string(f.readlines()))
+        return self._list_to_string(read_chapters)
 
     def get_sorted_chapters(self) -> list:
-        dir_list = os.listdir(self.path)
+        manuscript = os.listdir(self.path)
         return sorted(
-            [file for file in dir_list if re.match(r"\d\d_.*\.md|\d\d_.*\.txt", file)]
+            [file for file in manuscript if re.match(r"\d\d_.*\.md|\d\d_.*\.txt", file)]
         )
 
-    def _list_to_string(self, string_list: list) -> str:
-        return "".join(string_list)
+    def _list_to_string(self, list: list) -> str:
+        return "".join(list)

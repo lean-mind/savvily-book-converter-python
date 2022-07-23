e2038a45fae69a7b190af8303612b8b97521ed0a
Eric Driussi <eric@leanmind.com>
2022-07-23 18:56:45 +0100

    simplify fixtures

--- tests/test_acceptance_pre_processing.py
+++ tests/test_acceptance_pre_processing.py
@@ -6,10 +6,10 @@ import tests.fixtures.expected_output_md as expected
 
 class TestFormatter:
     def test_new_ebook_format(self):
-        assert (epubFormatter.run("tests/fixtures/sample-manuscript").read().decode() == expected.epub_format)
+        assert (epubFormatter.run("tests/fixtures/sample-manuscript").read().decode() == expected.basic_format)
 
     def test_new_pdf_print_format(self):
-        assert (printPdfFormatter.run("tests/fixtures/sample-manuscript").read().decode() == expected.pdf_print)
+        assert (printPdfFormatter.run("tests/fixtures/sample-manuscript").read().decode() == expected.pdf_print_format)
 
     def test_new_pdf_screen_format(self):
-        assert (screenPdfFormatter.run("tests/fixtures/sample-manuscript").read().decode() == expected.pdf_screen)
+        assert (screenPdfFormatter.run("tests/fixtures/sample-manuscript").read().decode() == expected.basic_format)

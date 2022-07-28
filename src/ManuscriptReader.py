b859a24c65da25366a8031261e1fdf03b9a115ed
Eric Driussi <eric@leanmind.com>
2022-07-28 18:20:30 +0100

    refactor reader

--- src/ManuscriptReader.py
+++ src/ManuscriptReader.py
@@ -3,26 +3,23 @@ import re
 
 
 class ManuscriptReader:
-    def __init__(self, path: str):
-        self.path = path
-
-    def read(self) -> str:
-        chapters_filenames = self.get_sorted_chapters()
+    def read(self, path: str) -> str:
+        chapters_filenames = self.get_sorted_chapters(path)
         read_chapters = []
         for file in chapters_filenames:
-            with open(f"{self.path}/{file}", "r") as f:
+            with open(f"{path}/{file}", "r") as f:
                 read_chapters.append(self._list_to_string(f.readlines()))
         return self._list_to_string(read_chapters)
 
-    def read_chapters(self, chapters_filenames: list) -> str:
+    def read_chapters(self, path: str, chapters_filenames: list) -> str:
         read_chapters = []
         for file in chapters_filenames:
-            with open(f"{self.path}/{file}", "r") as f:
+            with open(f"{path}/{file}", "r") as f:
                 read_chapters.append(self._list_to_string(f.readlines()))
         return self._list_to_string(read_chapters)
 
-    def get_sorted_chapters(self) -> list:
-        manuscript = os.listdir(self.path)
+    def get_sorted_chapters(self, path: str) -> list:
+        manuscript = os.listdir(path)
         return sorted([file for file in manuscript if re.match(r"\d\d_.*\.md|\d\d_.*\.txt", file)])
 
     def _list_to_string(self, list: list) -> str:
--- tests/test_manuscript_reader.py
+++ tests/test_manuscript_reader.py
@@ -4,17 +4,17 @@ import tests.fixtures.data.no_format_ch_01 as ch_01
 
 
 class TestReader:
-    reader = ManuscriptReader("tests/fixtures/sample-manuscript")
+    reader = ManuscriptReader()
 
     def test_files_are_sorted(self):
-        sorted_chapters = self.reader.get_sorted_chapters()
+        sorted_chapters = self.reader.get_sorted_chapters("tests/fixtures/sample-manuscript")
         expected_files = ["01_chapter.md", "02_chapter.md"]
         assert sorted_chapters == expected_files
 
     def test_whole_manuscript_is_read(self):
-        manuscript_content = self.reader.read()
+        manuscript_content = self.reader.read("tests/fixtures/sample-manuscript")
         assert manuscript_content == full_text.content
 
     def test_slected_chapters_are_read(self):
-        selected_chapters = self.reader.read_chapters(["01_chapter.md"])
+        selected_chapters = self.reader.read_chapters("tests/fixtures/sample-manuscript", ["01_chapter.md"])
         assert selected_chapters == ch_01.content

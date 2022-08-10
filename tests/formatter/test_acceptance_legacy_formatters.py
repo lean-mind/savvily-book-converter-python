d395dcb05fa54f2afa10c5afec8379d38839d8fb
Eric Driussi <eric@leanmind.com>
2022-08-10 17:15:07 +0100

    add fixtures for legacy formatter

--- tests/formatter/test_acceptance_legacy_formatters.py
+++ tests/formatter/test_acceptance_legacy_formatters.py
@@ -1,15 +1,16 @@
 import src.formatter.legacy.LegacyBasicFormatter as basicFormatter
 import src.formatter.legacy.LegacyPrintPDFFormatter as printPdfFormatter
-import tests.fixtures.data.legacy_basic_format_md as legacy_format_fixture
-import tests.fixtures.data.legacy_print_format_md as legacy_print_format_fixture
+import tests.formatter.fixtures.legacy_epub_format_md as epub_format_md
+import tests.formatter.fixtures.legacy_screen_pdf_format_md as screen_pdf_format_md
+import tests.formatter.fixtures.legacy_print_pdf_format_md as print_pdf_format_md
 
 
 class TestFormatter:
-    def test_new_ebook_format(self):
-        assert (basicFormatter.run("tests/fixtures/sample-manuscript").read().decode() == legacy_format_fixture.content)
+    def test_legacy_ebook_format(self):
+        assert (basicFormatter.run("tests/formatter/fixtures/sample-manuscript").read().decode() == epub_format_md.content)
 
-    def test_new_pdf_print_format(self):
-        assert (printPdfFormatter.run("tests/fixtures/sample-manuscript").read().decode() == legacy_print_format_fixture.content)
+    def test_legacy_pdf_print_format(self):
+        assert (printPdfFormatter.run("tests/formatter/fixtures/sample-manuscript").read().decode() == print_pdf_format_md.content)
 
-    def test_new_pdf_screen_format(self):
-        assert (basicFormatter.run("tests/fixtures/sample-manuscript").read().decode() == legacy_format_fixture.content)
+    def test_legacy_pdf_screen_format(self):
+        assert (basicFormatter.run("tests/formatter/fixtures/sample-manuscript").read().decode() == screen_pdf_format_md.content)

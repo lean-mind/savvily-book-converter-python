fd01bc07ed3e03321b10e9e011b22587aedf365d
Eric Driussi <eric@leanmind.com>
2022-08-10 16:33:27 +0100

    add domain fixtures

--- tests/domain/test_manuscript.py
+++ tests/domain/test_manuscript.py
@@ -1,22 +1,23 @@
 from src.domain.Manuscript import Manuscript
-import tests.fixtures.data.new_print_format_md as new_print_format_fixture
-import tests.fixtures.data.legacy_basic_format_md as legacy_format_fixture
-import tests.fixtures.data.no_format_md as full_text_fixture
+import tests.domain.fixtures.snapshots_v1_0.print_pdf_format_md as print_pdf_format
+import tests.domain.fixtures.snapshots_v1_0.screen_pdf_format_md as screen_pdf_format
+import tests.domain.fixtures.snapshots_v1_0.epub_format_md as epub_format
+import tests.domain.fixtures.unformatted_md as unformatted_md
 
 
 class TestManuscript:
 
     def test_epub_format(self):
-        broken_md = Manuscript(full_text_fixture.content)
+        broken_md = Manuscript(unformatted_md.content)
         formatted_md = broken_md.epub_format()
-        assert str(formatted_md) == legacy_format_fixture.content
+        assert str(formatted_md) == epub_format.content
 
     def test_screen_pdf_format(self):
-        broken_md = Manuscript(full_text_fixture.content)
+        broken_md = Manuscript(unformatted_md.content)
         formatted_md = broken_md.screen_pdf_format()
-        assert str(formatted_md) == legacy_format_fixture.content
+        assert str(formatted_md) == screen_pdf_format.content
 
     def test_print_pdf_format(self):
-        broken_md = Manuscript(full_text_fixture.content)
+        broken_md = Manuscript(unformatted_md.content)
         formatted_md = broken_md.print_pdf_format()
-        assert str(formatted_md) == new_print_format_fixture.content
+        assert str(formatted_md) == print_pdf_format.content
